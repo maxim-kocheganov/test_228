86dae490401aa1789bce9505e8b9157932c6fe90
max <maxim.k.elasnce@gmail.com>
2022-04-25 18:40:58 +0300

    asdasd

--- blog/views.py
+++ blog/views.py
@@ -1,6 +1,14 @@
 from django.shortcuts import render
+from django.contrib.auth.models import User
+from django.contrib.auth import authenticate
 
 # Create your views here.
 
 def login(request):
-    return render(request,"login.html")
+    if request.method =="GET":
+        return render(request,"login.html")
+    elif request.method =="POST":
+        user = request.POST["user"]
+        password = request.POST["password"]
+        user = authenticate(username=user, password=password)
+        pass
